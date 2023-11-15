01c210a74c851be5e05751dc2314f44bb2cbf004
Johnnie Walker <jw12866@gmail.com>
2023-11-15 07:40:45 -0500

    added periodic arr chart

--- src/visualization.py
+++ src/visualization.py
@@ -286,6 +286,160 @@ def create_monthly_mrr_chart(engine, start_date, end_date, customer=None, contra
     plt.savefig("exports/monthly_mrr.png", dpi=300)
 
 
+def create_arr_change_chart(engine, start_date, end_date, customer=None, contract=None, frequency='M'):
+    # NOT REALLY STARTED YET
+    
+    console = Console()
+    print_status(console, f"Creating ARR change chart between {start_date} and {end_date}", MessageStyle.INFO)
+    metrics_df = calc.populate_metrics_df(start_date, end_date, engine, customer, contract, frequency=frequency)
+
+    # Convert the index to DateTimeIndex if it's not already
+    #if not isinstance(metrics_df.index, pd.DatetimeIndex):
+    #    metrics_df.index = pd.to_datetime(metrics_df.index)
+
+    # Formatting the index to Year-Month
+    if frequency == 'Q':
+        periods = metrics_df.index
+    else:
+        periods = metrics_df.index.strftime('%Y-%m')
+
+    # Values for the bars
+    new_mrr = metrics_df['New MRR']
+    churn_mrr = -metrics_df['Churn MRR']
+    expansion_mrr = metrics_df['Expansion MRR']
+    contraction_mrr = -metrics_df['Contraction MRR']
+
+    aggregate_mrr = metrics_df['New MRR'] + metrics_df['Expansion MRR'] - metrics_df['Churn MRR'] - metrics_df['Contraction MRR']
+    
+    # Color for the bars
+    colors = [
+        COLOR_MAP["green_2"],
+        COLOR_MAP["green_1"],
+        COLOR_MAP["slate_blue"],
+        COLOR_MAP["sky_blue"]
+    ]
+
+    # Create the bar chart
+    fig, ax = plt.subplots(figsize=(12, 8))
+
+    # Plot the bars
+    ax.bar(periods, new_mrr, color=colors[0], label='New MRR')
+    ax.bar(periods, expansion_mrr, bottom=new_mrr, color=colors[1], label='Expansion MRR')
+    ax.bar(periods, churn_mrr, color=colors[2], label='Churn MRR')
+    ax.bar(periods, contraction_mrr, bottom=churn_mrr, color=colors[3], label='Contraction MRR')
+
+    # Add data value annotations
+    for i, (new, churn, expansion, contraction) in enumerate(zip(new_mrr, churn_mrr, expansion_mrr, contraction_mrr)):
+        if new != 0:
+            ax.text(i, new/2, f"${new/1000:,.0f}K", ha='center', va='center')
+        if expansion != 0:
+            ax.text(i, new + expansion/2, f"${expansion/1000:,.0f}K", ha='center', va='center')
+        if churn != 0:
+            ax.text(i, churn/2, f"${-churn/1000:,.0f}K", ha='center', va='center')
+        if contraction != 0:
+            ax.text(i, churn + contraction/2, f"${-contraction/1000:,.0f}K", ha='center', va='center')
+
+    # Add a line plot of aggregate MRR
+    ax.plot(periods, aggregate_mrr, color=COLOR_MAP["navy_blue"], linestyle='--', marker='o', label='Aggregate MRR Change')
+            
+    # Labeling and formatting
+    ax.set_ylabel('MRR', fontsize=14)
+    ax.set_xlabel('Month' if frequency == 'M' else 'Quarter', fontsize=14)
+    ax.set_title('Change in MRR by Month', fontsize=16)
+    ax.legend()
+    ax.yaxis.set_major_formatter(FuncFormatter(thousands_formatter))
+    ax.set_ylim(-40000, 40000)  # Setting y-axis min and max values
+
+    # Add a horizontal line at y=0
+    ax.axhline(0, color='black', linewidth=0.8)
+    
+    plt.xticks(rotation=45 if frequency == 'M' else 0)
+    plt.tight_layout()
+    plt.savefig("exports/mrr_change.png", dpi=300)
+    
+
+def create_monthly_arr_chart(engine, start_date, end_date, customer=None, contract=None, show_gridlines=False, frequency='M', ignoreoverrides=False):
+
+    # This function needs updated to sort the y axis limits to allow churn and contraction to be negative (need to check dataframe on that one)
+    # Would need more complex limits calculation
+    # For now have just excluded setting limits to see if good enough
+    
+    console = Console()
+    print_status(console, f"Creating periodic MRR chart between {start_date} and {end_date}", MessageStyle.INFO)
+    metrics_df = calc.populate_arr_metrics_df(start_date, end_date, engine, customer, contract, frequency=frequency)
+
+    # Convert the index to DateTimeIndex if it's not already
+    #if not isinstance(metrics_df.index, pd.DatetimeIndex):
+    #    metrics_df.index = pd.to_datetime(metrics_df.index)
+
+    # Formatting the index to Year-Month
+    if frequency == 'Q':
+        periods = metrics_df.index
+    else:
+        periods = metrics_df.index.strftime('%Y-%m')
+
+    # Values for the bars
+    starting_arr = metrics_df['Starting ARR']
+    new_arr = metrics_df['New ARR']
+    churn_arr = -metrics_df['Churn ARR']
+    expansion_arr = metrics_df['Expansion ARR']
+    contraction_arr = -metrics_df['Contraction ARR']
+
+    # Calculate the cumulative ARR value for each quarter, which is total of starting, expansion and so on, and find the max for the chart
+    max_abs_value = 0
+    for i, (starting, new, churn, expansion, contraction) in enumerate(zip(starting_arr, new_arr, churn_arr, expansion_arr, contraction_arr)):
+        if i == 0:
+            continue
+        cumulative = starting + new + churn + expansion + contraction
+        if abs(cumulative) > max_abs_value:
+            max_abs_value = abs(cumulative)
+    
+    # Round up the maximum absolute value to the nearest base
+    rounded_max_abs_value = max_abs_value + 50000
+
+    # Color for the bars
+    colors = [
+        COLOR_MAP["electric_green"],
+        COLOR_MAP["green_2"],
+        COLOR_MAP["green_1"],
+        COLOR_MAP["slate_blue"],
+        COLOR_MAP["sky_blue"]
+    ]
+
+    # Create the bar chart
+    fig, ax = plt.subplots(figsize=(12, 8))
+
+    # Plot the bars
+    ax.bar(periods, starting_arr, color=colors[0], label='Starting ARR')
+    ax.bar(periods, new_arr, bottom=starting_arr, color=colors[1], label='New ARR')
+    ax.bar(periods, expansion_arr, bottom=starting_arr + new_arr, color=colors[2], label='Expansion ARR')
+    ax.bar(periods, churn_arr, color=colors[3], label='Churn ARR')
+    ax.bar(periods, contraction_arr, bottom=churn_arr, color=colors[4], label='Contraction ARR')
+
+    # Labeling and formatting
+    ax.set_ylabel('ARR', fontsize=14)
+    ax.set_xlabel('Month' if frequency == 'M' else 'Quarter', fontsize=14)
+    ax.set_title('Monthly ARR' if frequency == 'M' else 'Quarterly ARR', fontsize=16)
+    ax.legend()
+    ax.yaxis.set_major_formatter(FuncFormatter(thousands_formatter))
+
+    # Set y-axis min and max values based on the rounded maximum absolute value
+    # ax.set_ylim(-20000, 60000)
+    # ax.set_ylim(0, rounded_max_abs_value)
+    
+    # Add gridlines if the show_gridlines argument is set to True
+    if show_gridlines:
+        ax.grid(which='major', axis='y', linestyle='--', linewidth=0.5)
+        ax.grid(which='major', axis='x', linestyle='--', linewidth=0.5)
+    
+    # Add a horizontal line at y=0
+    ax.axhline(0, color='black', linewidth=0.8)
+
+    plt.xticks(rotation=45 if frequency == 'M' else 0)
+    plt.tight_layout()
+    plt.savefig("exports/periodic_arr.png", dpi=300)
+
+    
 def create_bookings_arr_carr_chart(engine, start_date, end_date, customer=None, contract=None, show_gridlines=False, frequency='M', ignoreoverrides=False):
 
     console = Console()
